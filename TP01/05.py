88dbdc8add26d6b529fd7985c7c30ad5e7268812
LuchoLatella <lucho_latella@hotmail.com>
2024-04-21 20:47:40 -0300

    tp1 hechos 5

---
+++ TP01/05.py
@@ -0,0 +1,7 @@
+nombre = input ("Ingrese su nombre: ")
+numero_1 = float(input ("Bienvenido " + nombre + ", ingrese un número: " ))
+numero_2 = float(input ("Ingrese otro número por favor: "))
+numero_3 = float(input ("Ingrese un tercer valor: "))
+resultado_suma = numero_1 + numero_2
+resultado_multi = resultado_suma * numero_3
+print ("El resultado final de las dos operaciones es: " , resultado_multi)
