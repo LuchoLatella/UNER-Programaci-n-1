745d5b16d16e431964cb9d9b9376600365c881d6
LuchoLatella <lucho_latella@hotmail.com>
2024-04-21 20:41:02 -0300

    tp1 hechos 4

---
+++ TP01/04.py
@@ -0,0 +1,5 @@
+nombre = input ("Ingrese su nombre: ")
+numero_1 = float(input ("Bienvenido " + nombre + ", ingrese un número: " ))
+numero_2 = float(input ("Ingrese otro número por favor: "))
+resultado_suma = numero_1 + numero_2
+print (nombre + ", la suma de ambos números, es " , resultado_suma)
