8f0ce9ff668c421504ebcb2f3c387683cdabe732
LuchoLatella <lucho_latella@hotmail.com>
2024-04-21 20:27:48 -0300

    tp1 hechos 2 y 3

---
+++ TP01/02.py
@@ -0,0 +1,2 @@
+nombre_usuario = input ("Por favor, ingrese el nombre del usuario:")
+print ("Hola " + nombre_usuario)
---
+++ TP01/03.py
@@ -0,0 +1,3 @@
+nombre = input ("Bienvenido, ingrese su nombre: ")
+apellido = input (" por favor, ingrese su apellido: ")
+print ("Hola " , nombre , apellido)
