162460c50c9c06787ac18494c9363fa20bd1cad6
LuchoLatella <lucho_latella@hotmail.com>
2024-04-22 20:09:45 -0300

    tp1 hechos hasta el  8

---
+++ TP01/08.py
@@ -0,0 +1,9 @@
+#Escriba un programa que permita al usuario ingresar la base y altura de un triángulo para luego imprimir por pantalla la superficie total.
+
+nombre = input ("Ingrese su nombre: ")
+base_triangulo = float(input ("Bienvenido " + nombre + ", ingrese el tamaño de la base: " ))
+altura_triangulo = float(input ("Ingrese el valor de altura: "))
+
+superficie_triangulo = (base_triangulo * altura_triangulo) / 2
+
+print (nombre + ", la superficie del triángulo es: " , superficie_triangulo)
