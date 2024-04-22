e61bc39befe0b5d12a5efe7828a48b3ecc80c5bf
LuchoLatella <lucho_latella@hotmail.com>
2024-04-22 20:41:17 -0300

    tp1 hechos hasta el  10

---
+++ TP01/10.py
@@ -0,0 +1,12 @@
+#Escriba un programa que indique si un texto es palíndromo, es decir, se escribe igual al 
+#derecho que al revés. Por ejemplo: rayar, kayak, somos.
+
+texto = input ("Ingrese el texto a verificar si es palíndromo: ")
+palindromo = texto == texto [::-1]
+
+if palindromo:
+    print("El texto " + texto + " es un palíndromo")
+else:
+    print ("El texto " + texto + " no es un palíndromo")
+
+#print ("Los textos son: " , texto , "y" , palindromo)
