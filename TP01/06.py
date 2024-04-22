a566d48d2848f1f2f3ca0181a0408bf53ef3a5b1
LuchoLatella <lucho_latella@hotmail.com>
2024-04-21 21:00:58 -0300

    tp1 hechos 6

---
+++ TP01/06.py
@@ -0,0 +1,5 @@
+nombre_1 = input ("Ingrese su nombre por favor: ")
+total_cuenta = float(input ("Binevenido " + nombre_1 + " Ingrese el valor total de la cuenta: "))
+comensales = int(input ("Cantidad de comensales que participaron: "))
+costo_individuo = total_cuenta / comensales
+print ("El costo a abonar por comensal es: " , costo_individuo)
