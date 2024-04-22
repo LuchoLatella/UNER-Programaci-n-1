11ac81ecc9ebb3907e109d31d5a85fce4faf5980
LuchoLatella <lucho_latella@hotmail.com>
2024-04-22 19:53:44 -0300

    tp1 hechos hasta el  7

---
+++ TP01/07.py
@@ -0,0 +1,9 @@
+#Pida al usuario un número x de días y luego mostrar por pantalla cuántas horas, minutos y segundos son esos números de días.
+
+nombre_1 = input ("Ingrese su nombre por favor: ")
+dias_usuario = int(input ("Binevenido " + nombre_1 + ", ingrese la cantidad de días: "))
+cantidad_horas = dias_usuario * 24
+cantidad_minutos = cantidad_horas * 60
+cantidad_segundos = cantidad_minutos * 60
+
+print ("En total, señor " + nombre_1 + " han transcurrido " , cantidad_horas, "horas, ", cantidad_minutos, "minutos y ", cantidad_segundos, "segundos.-")
