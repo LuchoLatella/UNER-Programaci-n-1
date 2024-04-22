017293b3cf7885837d834671a3815502ae428a7b
LuchoLatella <lucho_latella@hotmail.com>
2024-04-22 20:45:05 -0300

    tp1 hechos hasta el  10

--- TP01/09.py
+++ TP01/09.py
@@ -1,4 +1,4 @@
 #Pida al usuario que ingrese un texto para luego imprimirlo al revés. Ej: HOLA -> ALOH.
 
-texto_usuario = input ("Por favor, ingrese el texto deseado: ")
+texto_usuario = input ("Por favor, ingrese el texto deseado: ").upper()
 print (texto_usuario [::-1])
--- TP01/10.py
+++ TP01/10.py
@@ -1,7 +1,7 @@
 #Escriba un programa que indique si un texto es palíndromo, es decir, se escribe igual al 
 #derecho que al revés. Por ejemplo: rayar, kayak, somos.
 
-texto = input ("Ingrese el texto a verificar si es palíndromo: ")
+texto = input ("Ingrese el texto a verificar si es palíndromo: ").upper()
 palindromo = texto == texto [::-1]
 
 if palindromo:
