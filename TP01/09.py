50826c4c4a524a5c957aeb3eb2cf578994397ce1
LuchoLatella <lucho_latella@hotmail.com>
2024-04-22 20:23:44 -0300

    tp1 hechos hasta el  9

---
+++ TP01/09.py
@@ -0,0 +1,4 @@
+#Pida al usuario que ingrese un texto para luego imprimirlo al revés. Ej: HOLA -> ALOH.
+
+texto_usuario = input ("Por favor, ingrese el texto deseado: ")
+print (texto_usuario [::-1])
