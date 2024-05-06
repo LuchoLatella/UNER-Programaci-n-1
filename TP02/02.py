cb0ff8986768eff59f8103ce2688a35d5b881954
LuchoLatella <lucho_latella@hotmail.com>
2024-05-06 20:54:17 -0300

    ejercicio 2  TP02

---
+++ TP02/02.py
@@ -0,0 +1,13 @@
+# Que reciba un número entero positivo y una potencia a elevar y que nos devuelva el resultado
+
+def calculo_pontecia (entero,potencia):
+    if entero < 0:
+        return "Intente nuevamente con un número entero positivo"
+    resultado = entero ** potencia 
+    return resultado
+
+entero = int(input ("Ingrese un número entero: "))
+potencia = int(input ("ingrese una potencia :"))
+
+resultado = calculo_pontecia(entero,potencia)
+print(f"El resultado de", entero, "elevado a la potencia", potencia , "es:", resultado)
