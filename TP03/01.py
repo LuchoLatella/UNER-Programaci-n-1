9e54c7dc50308e3398824c84ff5b2acbf21b21a8
LuchoLatella <lucho_latella@hotmail.com>
2024-05-30 19:05:08 -0300

    agregado ejercicio 1 tp03

---
+++ TP03/01.py
@@ -0,0 +1,5 @@
+# Crear un programa que almacene en una lista las materias de esta u otra carrera y que las
+# muestre por pantalla. (La lista debe ser predefinida, no debe ser ingresada por el usuario).
+
+materias = ["programación 1", "introducción a la informática", "diseño", "programación 2"]
+print(f"las materias incluídas son:", materias)
