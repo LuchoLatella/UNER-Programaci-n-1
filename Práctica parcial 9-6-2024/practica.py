5024e546b4e6c87e30dc49fac546cc346d1d561f
LuchoLatella <lucho_latella@hotmail.com>
2024-06-17 22:08:59 -0300

    inicio práctica parcial 18/6/2024

--- Práctica parcial 9-6-2024/practica.py
+++ Práctica parcial 9-6-2024/practica.py
@@ -3,13 +3,39 @@
 
 # Muestre por pantalla el estado en el que se encuentra esa persona en función del valor de IMC: 
 
-peso = int(input("ingrese el peso a considerar: "))
-altura = float(input("por favor, ingrese la altura a considerar: "))
+
 
 def calculo_imc (peso, altura):
-    imc = peso / altura**2
-    print("El IMC de la persona es ", imc)
+    imc = peso / (altura**2)
+    return imc    # print("El IMC de la persona es ", imc)
 
-calculo_imc(peso, altura)
+def estado_imc (imc):
+    if imc < 18.5:
+        return("Bajo peso")
+    elif imc >= 18.5 and imc <= 24.9:
+        return("Normal")
+    elif imc >= 25 and imc <= 29.9:
+        return("Sobrepeso")
+    elif imc >= 30 and imc <= 39.9:
+        return("Obesidad")
+    else:
+        return("Obesidad morbida")
+
+
+def mostrar_datos_imc():
+    try:
+        peso = float(input("ingrese el peso a considerar: "))
+        altura = float(input("por favor, ingrese la altura a considerar: "))
+    except ValueError:
+        print("por favor, ingresar valores válidos")
+        return
+
+    imc = calculo_imc (peso, altura)
+    
+    print("El IMC de la persona es", imc)
+    print("El estado de la persona es", estado_imc(imc))
+
+mostrar_datos_imc()
+# calculo_imc(peso, altura)
 
 
