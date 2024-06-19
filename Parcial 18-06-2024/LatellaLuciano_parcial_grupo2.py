8634ddda731a2a4e7ee8bae93de2db7008a2513f
LuchoLatella <lucho_latella@hotmail.com>
2024-06-18 21:20:52 -0300

    inicio parcial grupo 2d 18/6/2024

--- Parcial 18-06-2024/LatellaLuciano_parcial_grupo2.py
+++ Parcial 18-06-2024/LatellaLuciano_parcial_grupo2.py
@@ -13,6 +13,17 @@ def temperatura_diaria_usuario():
     promedio_temeperaturas = sum(temperaturas) / len(temperaturas)
     print(f"\nEl promedio de la temperatura de la semana es: {promedio_temeperaturas} °C.")
 
+    temperaturas_cadena = ', '.join(map(str, temperaturas))
+    print(f"\nTodas las temperaturas como cadena separadas por comas: {temperaturas_cadena}")
+
+    dias_calurosos = 0
+    for grados in temperaturas:
+        if grados >= 24:
+            dias_calurosos += 1
+    
+    print(f"\nCantidad de días con temperatura mayor o igual a 24 °C: {dias_calurosos}")
+
+
 
 
     
